3535e28ac8ef5d73712ddb80472a715fe2653c5d
wtfisquantum <92854101+wtfisquantum@users.noreply.github.com>
2026-08-07 00:50:13 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import HowItWorks from './howitworks.tsx'
+import TechVerseApp from './decode.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/how-it-works" element={<HowItWorks />} />
+        <Route path="/decode" element={<TechVerseApp />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
